307b813c4812ab8151cd32ecefa3532431b8ca6f
jc400 <105320127+jc400@users.noreply.github.com>
2022-08-04 17:38:44 -0600

    debugging stuff for keypresses again

--- MAIN.py
+++ MAIN.py
@@ -97,7 +97,7 @@ class Game:
 
     def on_keypress(self, event):
         """Callback. Translates key to direction, updates xdir/ydir"""
-        print('k', end='')
+        print('p', end='')
         kp = event.keysym.lower()
         direction_dict = {'a':'left',
                           'd':'right',
@@ -115,6 +115,7 @@ class Game:
 
     def on_keyrelease(self, event):
         """Ensures direction continues until let go"""
+        print('r', end='')
         kr = event.keysym.lower()
         if (kr == 'a' and self.control.xdir == 'left') or (kr == 'd' and self.control.xdir == 'right'):
             self.control.xdir = None
