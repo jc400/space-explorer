b66205f329906ce430600e011d1d41612d11eb56
jc400 <105320127+jc400@users.noreply.github.com>
2022-08-04 11:55:34 -0600

    moved control config back into control.py. No need for external file

--- control.py
+++ control.py
@@ -1,16 +1,25 @@
 #CONTROL
-
-from data.controlconfig import *    #importing movement constants. This is ugly I kno
-
 import tkinter
 import os
 
- 
+
+# MOVEMENT CONSTANTS
+FALL_ACCELERATION = 1   #this is a rate of change.
+
+JUMP_MOMENTUM = 10#8       #this is constant momentum through the jump
+JUMP_FRAMES = 12#15
+   
+JETPACK_ACCELERATION = 1 #full accel applied when falling. Half applied when rising.
+JETPACK_REGEN = 0.15
+JETPACK_MAX = 24
+
+RUN_ACCELERATION = 0.5
+X_BRAKE_AIR = 1
+X_BRAKE_GROUND = 2
+X_FRICTION_GROUND = .4
 
 
 class Control:
-
-
 #----------INIT AND SETUP---------------#
 
     def __init__(self, parent, screen):
